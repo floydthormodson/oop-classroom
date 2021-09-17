79a05872eab1955e99649fd4e450cb7d0e06a2e1
floydthormodson <floydthormodson@outlook.com>
2021-09-17 11:50:37 -0400

    Deactivated desk Class test, activated marker and room Class tests, committing passable room Class, committing partially passable marker Class

--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,44 @@
 class Marker {
+  constructor(size, color, remainingInk){
+      this.size =size
+      this.color = color
+      this.remainingInk = remainingInk
+  }
   
+  write(word){
+    let charsPrint = word
+    let totalChars = word.length
+    let totalCharsArray = word.split('')
+    let inkChars = totalCharsArray.filter(char=> {return char = ' '})
+    
+
+    if(this.remainingInk>inkChars.length){
+        this.remainingInk = this.remainingInk - inkChars.length
+        return charsPrint
+    }else{
+       // let num =this.remainingInk
+        return charsPrint.substring(0,this.remainingInk)
+    }
+
+
+
+
+
+
+
+
+
+    /*let inkWords= sentence.reduce(char=> char=' ')
+    for(i=0; i<=inkWords.length; i++){
+        if(this.remainingInk >= inkWords.length){
+            this.remainingInk--
+        }else if(this.remainingInk<inkWords.length){
+
+        }
+    }*/
+  }
+      
+      
 }
 
 module.exports = Marker
--- src/room.js
+++ src/room.js
@@ -1,4 +1,20 @@
 class Room {
+  constructor(name, description){
+      this.name =name
+      this.description = description
+      this.contents = []
+  }
+  add(item){
+      this.contents.push(item)
+      return this
+  }
+  has(ref){
+    if(this.contents.includes(ref)){
+        return true
+    }else{
+        return false
+    }
+  }
   
 }
 
