4ab34b64a6bfb58dc8d4af2e2484f2c6a81758c3
floydthormodson <floydthormodson@outlook.com>
2021-09-17 14:37:50 -0400

    committed partial marker and partial student

--- src/marker.js
+++ src/marker.js
@@ -6,39 +6,14 @@ class Marker {
   }
   
   write(word){
-    let charsPrint = word
-    let totalChars = word.length
-    let totalCharsArray = word.split('')
-    let inkChars = totalCharsArray.filter(char=> {return char = ' '})
+      let charCount = word.split('').filter(char=>char!== ' ')
+
+      this.remainingInl -= charcterCount;
+      return word
+
+      //if(charCount>this)
     
-
-    if(this.remainingInk>inkChars.length){
-        this.remainingInk = this.remainingInk - inkChars.length
-        return charsPrint
-    }else{
-       // let num =this.remainingInk
-        return charsPrint.substring(0,this.remainingInk)
-    }
-
-
-
-
-
-
-
-
-
-    /*let inkWords= sentence.reduce(char=> char=' ')
-    for(i=0; i<=inkWords.length; i++){
-        if(this.remainingInk >= inkWords.length){
-            this.remainingInk--
-        }else if(this.remainingInk<inkWords.length){
-
-        }
-    }*/
-  }
-      
-      
+}
 }
 
 module.exports = Marker
--- src/student.js
+++ src/student.js
@@ -1,4 +1,30 @@
 class Student {
+  constructor(name, skillLevel){
+      this.name = name
+      this.skillLevel = skillLevel
+      this.assignments = []
+  }
+  study(){
+      if(this.skillLevel<100){
+          this.skillLevel++
+      }
+      return this
+  }
+
+  
+  doHomework(homework){
+      
+      if(homework.skillLevel<this.skillLevel){
+          homework.completed = true
+      }else{
+          homework.completed=false
+      }
+      if(homework.skillLevel != undefined && homework.skillLevel>1 && homework.skillLevel<=100){
+        this.assignments.push(homework)
+      }
+      
+
+    }
   
 }
 
