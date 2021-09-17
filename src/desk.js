f5b67b78a5a6df67ba86c250ceae60cc7a12336c
floydthormodson <floydthormodson@outlook.com>
2021-09-17 10:21:42 -0400

    Deactivated computer object test, activated desk object test, committed working desk Class object

--- src/desk.js
+++ src/desk.js
@@ -1,5 +1,21 @@
 class Desk {
-  
+  constructor(length, width, height, isWhiteboard){
+      this.length = length
+      this.width = width
+      this.height =height
+      this.isWhiteboard = isWhiteboard
+      this.content= ''
+  }
+  write(words){
+      return this.content += words
+  }
+  wipe(){
+      if(this.isWhiteboard){
+          this.content = ''  
+      }else{
+          this.content = this.content
+      }
+  }
 }
 
 module.exports = Desk
